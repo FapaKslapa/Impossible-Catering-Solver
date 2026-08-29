3ee1bf5995ab73bbbf3d48ff8eca1034a5ae29f4
fapakslapa <stefanomarocco0@gmail.com>
2026-08-29 10:16:41 +0200

    build: add Lode.c amalgamation and CMake C11 support

---
+++ build_lode.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+OUT=Lode.c
+: > "$OUT"
+
+HEADERS="src/line_reader.h src/token_scanner.h src/dish_table.h src/formula.h src/formula_parser.h src/sat_solver.h src/sat_internal.h src/threshold_search.h src/result_printer.h"
+SOURCES="src/line_reader.c src/token_scanner.c src/dish_table.c src/formula.c src/formula_parser.c src/sat_propagate.c src/sat_solver.c src/threshold_search.c src/result_printer.c src/main.c"
+
+for f in $HEADERS; do
+    grep -v '^#include "' "$f" >> "$OUT"
+    printf '\n' >> "$OUT"
+done
+
+for f in $SOURCES; do
+    grep -v '^#include "' "$f" >> "$OUT"
+    printf '\n' >> "$OUT"
+done
+
+echo "Wrote $OUT"
